9ff6225996a67bd6f0a14f37a1d8a044bf6386cd
Jose Pirir <josepirirromero@gmail.com>
2022-07-09 13:11:27 -0600

    test

--- Entities/Aspirante.cs
+++ Entities/Aspirante.cs
@@ -7,7 +7,7 @@ namespace WebApiKalum.Entities
     public class Aspirante //: IValidatableObject
     {
         [Required(ErrorMessage ="El campo {0} es requerido")]
-        [StringLength(12, MinimumLength = 12, ErrorMessage =" El campo número de expediente debe de ser de 12 caracteres")]
+        [StringLength(11, MinimumLength = 11, ErrorMessage =" El campo número de expediente debe de ser de 11 caracteres")]
         [NoExpediente]
         public string NoExpediente {get; set;}
         [Required(ErrorMessage ="El campo {0} es requerido")]
